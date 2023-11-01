e384f067b76b96a9e673186151f20b19b17b1945
Jason Roy <jason@mrjasonroy.com>
2023-10-31 20:50:30 -0700

    fixes to sql issues

--- auth.ts
+++ auth.ts
@@ -24,11 +24,14 @@ export const { auth, signIn, signOut } = NextAuth({
         const parsedCredentials = z
           .object({ email: z.string().email(), password: z.string().min(6) })
           .safeParse(credentials);
- 
+        
         if (parsedCredentials.success) {
           const { email, password } = parsedCredentials.data;
           const user = await getUser(email);
           if (!user) return null;
+          const passwordsMatch = await bcrypt.compare(password, user.password);
+ 
+          if (passwordsMatch) return user;
         }
  
         return null;
